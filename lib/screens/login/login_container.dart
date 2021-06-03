88e92e75409621abad2161210cf8a4581651e959
Nguyen Truong Duy <truongduy.2611@gmail.com>
2021-06-03 11:34:10 +0700

    Fixed: safe area for login and register

--- lib/screens/login/login_container.dart
+++ lib/screens/login/login_container.dart
@@ -58,87 +58,97 @@ class _LoginContainerState extends State<LoginContainer> {
               vertical: 16.0,
               horizontal: 16.0,
             ),
-            sliver: SliverToBoxAdapter(
-              child: ReactiveForm(
-                formGroup: _formGroup,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const SizedBox(height: 16.0),
-                    ReactiveTextField(
-                      formControlName: 'email',
-                      autocorrect: false,
-                      validationMessages: (control) => {
-                        ValidationMessage.required:
-                            S.current.emailIsRequiredError,
-                        ValidationMessage.email: S.current.emailIsInvalidError,
-                      },
-                      decoration: InputDecoration(
-                        hintText: S.current.emailLabel,
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    ReactiveTextField(
-                      formControlName: 'password',
-                      obscureText: true,
-                      autocorrect: false,
-                      validationMessages: (control) => {
-                        ValidationMessage.required:
-                            S.current.passwordIsRequiredError
-                      },
-                      decoration: InputDecoration(
-                        hintText: S.current.passwordLabel,
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    BlocListener<LoginBloc, LoginState>(
-                      listener: (context, state) {
-                        if (state is LoginError) {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16.0),
-                              ),
-                              title: Text(state.message),
-                              actionsPadding: const EdgeInsets.all(12.0),
-                              actions: [
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: FilledAppButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text(S.current.okTitle),
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        }
-                        if (state is LoginSuccess) {
-                          /// Pop the logout page then replace
-                          /// by the loggedInRoot
-                          Navigator.pop(context);
-                          Navigator.pushReplacementNamed(
-                              context, Routes.loggedInRoot);
-                        }
-                      },
-                      child: BlocBuilder<LoginBloc, LoginState>(
-                        builder: (context, state) {
-                          return FilledAppButton(
-                            onPressed:
-                                state is LoginInProgress ? null : _onSubmit,
-                            child: state is LoginInProgress
-                                ? MiniCircularProgressIndicator(
-                                    color: theme.colorScheme.onPrimary,
-                                  )
-                                : Text(S.current.loginButtonText),
-                          );
+            sliver: SliverSafeArea(
+              top: false,
+              sliver: SliverToBoxAdapter(
+                child: ReactiveForm(
+                  formGroup: _formGroup,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(height: 16.0),
+                      ReactiveTextField(
+                        formControlName: 'email',
+                        autocorrect: false,
+                        validationMessages: (control) => {
+                          ValidationMessage.required:
+                              S.current.emailIsRequiredError,
+                          ValidationMessage.email:
+                              S.current.emailIsInvalidError,
+                        },
+                        decoration: InputDecoration(
+                          hintText: S.current.emailLabel,
+                        ),
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.emailAddress,
+                        onSubmitted: () {
+                          _formGroup.control('password').focus();
                         },
                       ),
-                    )
-                  ],
+                      const SizedBox(height: 16.0),
+                      ReactiveTextField(
+                        formControlName: 'password',
+                        obscureText: true,
+                        autocorrect: false,
+                        validationMessages: (control) => {
+                          ValidationMessage.required:
+                              S.current.passwordIsRequiredError
+                        },
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          hintText: S.current.passwordLabel,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      BlocListener<LoginBloc, LoginState>(
+                        listener: (context, state) {
+                          if (state is LoginError) {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                title: Text(state.message),
+                                actionsPadding: const EdgeInsets.all(12.0),
+                                actions: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width,
+                                    child: FilledAppButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text(S.current.okTitle),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }
+                          if (state is LoginSuccess) {
+                            /// Pop the logout page then replace
+                            /// by the loggedInRoot
+                            Navigator.pop(context);
+                            Navigator.pushReplacementNamed(
+                                context, Routes.loggedInRoot);
+                          }
+                        },
+                        child: BlocBuilder<LoginBloc, LoginState>(
+                          builder: (context, state) {
+                            return FilledAppButton(
+                              onPressed:
+                                  state is LoginInProgress ? null : _onSubmit,
+                              child: state is LoginInProgress
+                                  ? MiniCircularProgressIndicator(
+                                      color: theme.colorScheme.onPrimary,
+                                    )
+                                  : Text(S.current.loginButtonText),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/screens/register/register_container.dart
+++ lib/screens/register/register_container.dart
@@ -61,143 +61,148 @@ class _RegisterContainerState extends State<RegisterContainer> {
               vertical: 16.0,
               horizontal: 16.0,
             ),
-            sliver: SliverToBoxAdapter(
-              child: ReactiveForm(
-                formGroup: _formGroup,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const SizedBox(height: 16.0),
-                    ReactiveTextField(
-                      formControlName: 'email',
-                      autocorrect: false,
-                      validationMessages: (control) => {
-                        ValidationMessage.required:
-                            S.current.emailIsRequiredError,
-                        ValidationMessage.email: S.current.emailIsInvalidError,
-                      },
-                      onSubmitted: () {
-                        _formGroup.control('password').focus();
-                      },
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        hintText: S.current.emailLabel,
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    ReactiveTextField(
-                      formControlName: 'password',
-                      obscureText: true,
-                      autocorrect: false,
-                      validationMessages: (control) => {
-                        ValidationMessage.required:
-                            S.current.passwordIsRequiredError
-                      },
-                      onSubmitted: () {
-                        _formGroup.control('confirm').focus();
-                      },
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: InputDecoration(
-                        hintText: S.current.passwordLabel,
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    ReactiveTextField(
-                      formControlName: 'confirm',
-                      obscureText: true,
-                      autocorrect: false,
-                      keyboardType: TextInputType.visiblePassword,
-                      validationMessages: (control) => {
-                        ValidationMessage.mustMatch:
-                            S.current.confirmPasswordIsNotMatchError
-                      },
-                      decoration: InputDecoration(
-                        hintText: S.current.confirmPasswordLabel,
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    Text.rich(
-                      TextSpan(
-                        text: S.current.termOfServiceAndPrivacyPolicyText,
-                        children: [
-                          WidgetSpan(
-                            child: InkWell(
-                              onTap: () {},
-                              child: Text(
-                                S.current.termOfServiceText,
-                                style: const TextStyle(
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                            ),
-                          ),
-                          TextSpan(text: ' ${S.current.andText} '),
-                          WidgetSpan(
-                            child: InkWell(
-                              onTap: () {},
-                              child: Text(
-                                S.current.privacyPolicyText,
-                                style: const TextStyle(
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const TextSpan(text: '.'),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    BlocListener<RegisterBloc, RegisterState>(
-                      listener: (context, state) {
-                        if (state is RegisterError) {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16.0),
-                              ),
-                              title: Text(state.message),
-                              actionsPadding: const EdgeInsets.all(12.0),
-                              actions: [
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: FilledAppButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text(S.current.okTitle),
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        }
-                        if (state is RegisterSuccess) {
-                          /// Pop the logout page then replace
-                          /// by the loggedInRoot
-                          Navigator.pop(context);
-                          Navigator.pushReplacementNamed(
-                              context, Routes.loggedInRoot);
-                        }
-                      },
-                      child: BlocBuilder<RegisterBloc, RegisterState>(
-                        builder: (context, state) {
-                          return FilledAppButton(
-                            onPressed:
-                                state is RegisterInProgress ? null : _onSubmit,
-                            child: state is RegisterInProgress
-                                ? MiniCircularProgressIndicator(
-                                    color: theme.colorScheme.onPrimary,
-                                  )
-                                : Text(S.current.registerButtonText),
-                          );
+            sliver: SliverSafeArea(
+              top: false,
+              sliver: SliverToBoxAdapter(
+                child: ReactiveForm(
+                  formGroup: _formGroup,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(height: 16.0),
+                      ReactiveTextField(
+                        formControlName: 'email',
+                        autocorrect: false,
+                        validationMessages: (control) => {
+                          ValidationMessage.required:
+                              S.current.emailIsRequiredError,
+                          ValidationMessage.email:
+                              S.current.emailIsInvalidError,
                         },
+                        onSubmitted: () {
+                          _formGroup.control('password').focus();
+                        },
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: S.current.emailLabel,
+                        ),
                       ),
-                    )
-                  ],
+                      const SizedBox(height: 16.0),
+                      ReactiveTextField(
+                        formControlName: 'password',
+                        obscureText: true,
+                        autocorrect: false,
+                        validationMessages: (control) => {
+                          ValidationMessage.required:
+                              S.current.passwordIsRequiredError
+                        },
+                        onSubmitted: () {
+                          _formGroup.control('confirm').focus();
+                        },
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          hintText: S.current.passwordLabel,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      ReactiveTextField(
+                        formControlName: 'confirm',
+                        obscureText: true,
+                        autocorrect: false,
+                        keyboardType: TextInputType.visiblePassword,
+                        validationMessages: (control) => {
+                          ValidationMessage.mustMatch:
+                              S.current.confirmPasswordIsNotMatchError
+                        },
+                        decoration: InputDecoration(
+                          hintText: S.current.confirmPasswordLabel,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      Text.rich(
+                        TextSpan(
+                          text: S.current.termOfServiceAndPrivacyPolicyText,
+                          children: [
+                            WidgetSpan(
+                              child: InkWell(
+                                onTap: () {},
+                                child: Text(
+                                  S.current.termOfServiceText,
+                                  style: const TextStyle(
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            TextSpan(text: ' ${S.current.andText} '),
+                            WidgetSpan(
+                              child: InkWell(
+                                onTap: () {},
+                                child: Text(
+                                  S.current.privacyPolicyText,
+                                  style: const TextStyle(
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const TextSpan(text: '.'),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      BlocListener<RegisterBloc, RegisterState>(
+                        listener: (context, state) {
+                          if (state is RegisterError) {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                title: Text(state.message),
+                                actionsPadding: const EdgeInsets.all(12.0),
+                                actions: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width,
+                                    child: FilledAppButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text(S.current.okTitle),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }
+                          if (state is RegisterSuccess) {
+                            /// Pop the logout page then replace
+                            /// by the loggedInRoot
+                            Navigator.pop(context);
+                            Navigator.pushReplacementNamed(
+                                context, Routes.loggedInRoot);
+                          }
+                        },
+                        child: BlocBuilder<RegisterBloc, RegisterState>(
+                          builder: (context, state) {
+                            return FilledAppButton(
+                              onPressed: state is RegisterInProgress
+                                  ? null
+                                  : _onSubmit,
+                              child: state is RegisterInProgress
+                                  ? MiniCircularProgressIndicator(
+                                      color: theme.colorScheme.onPrimary,
+                                    )
+                                  : Text(S.current.registerButtonText),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
